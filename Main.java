acd2025d800b69baf4c21bdf3dce2c34329a2435
110694784AB <110694784@ea.edin.sch.uk>
2021-01-14 10:09:53 +0000

    I wrote the program

--- Main.java
+++ Main.java
@@ -2,10 +2,10 @@ class Main {
   public static void main(String[] args) {
     
     // ask user for the words and store as message
-
+String name = Keyboard.getText("Please enter your name");
     // store the number of the letters as numberLetters
-    
+  int nameLength = name.length();  
     // display "There are " + numberLetters + " in " + message
-
+Screen.display("There are " + nameLength + " characters in the name: " + name, "Name Character Counter");
   }
 }
